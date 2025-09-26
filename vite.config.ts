d7620a099c28f628de28f9b3fe2fa66d24f97294
Miguel Almeida <miguel.almeida@mercedes-benz.io>
2025-09-26 17:14:48 +0100

    Fix Vite path (again)

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< Updated upstream
-  base: './KartTracker/',
-=======
   base: 'KartTracker',
->>>>>>> Stashed changes
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
